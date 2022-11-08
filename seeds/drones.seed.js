2ba7e158e941221021e33300be74dfeef641f48c
Amélie Chabrand <amelie.chabrand@gmail.com>
2022-11-08 17:08:55 +0100

    all iterations done

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,28 @@
-// Iteration #1
+require("dotenv/config");
+require("./../db");
+const Drone = require("./../models/Drone.model.js");
+
+const drones = [
+  {
+    name: "DarkStar",
+    propellers: 2,
+    maxSpeed: 16,
+  },
+  {
+    name: "Global Hawk",
+    propellers: 4,
+    maxSpeed: 20,
+  },
+  {
+    name: "Hunter",
+    propellers: 3,
+    maxSpeed: 18,
+  },
+];
+
+seeds();
+
+async function seeds() {
+  await Drone.deleteMany();
+  await Drone.create(drones);
+}
